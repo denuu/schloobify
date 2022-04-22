8cf081b3c04dcad7f1a1de8ef41af90629ec0d8d
Denis Nossevitch <denis.nossevitch@gmail.com>
2022-04-22 12:20:49 -0700

    signup and signin pages

---
+++ lib/fetcher.ts
@@ -0,0 +1,11 @@
+// NOTE: JSON stringify doesn't set undefined but does set null.
+export default function fetcher(url: string, data = undefined) {
+	return fetch(`${window.location.origin}/api${url}`, {
+		method: data ? 'POST' : 'GET',
+		credentials: 'include',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),
+	})
+}
---
+++ pages/signin.tsx
@@ -0,0 +1,10 @@
+import { MdSignalWifiStatusbarNull } from 'react-icons/md'
+import AuthForm from '../components/authForm'
+
+const Signin = () => {
+    return <AuthForm mode="signin" />
+}
+
+Signin.authPage = true
+
+export default Signin
