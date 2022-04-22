21f381d0b1743473de204249016d7757e2aa8b45
Denis Nossevitch <denis.nossevitch@gmail.com>
2022-04-22 12:59:53 -0700

    token validation

---
+++ lib/auth.ts
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken'
+import { NextApiRequest, NextApiResponse } from 'next'
+import prisma from './prisma'
+
+// Higher Order Function that wraps our handlers
+export const validateRoute = (handler) => {
+	return async (req: NextApiRequest, res: NextApiResponse) => {
+		const token = req.cookies.SCHLOOBIFY_ACCESS_TOKEN
+
+		if (token) {
+			let user
+
+			try {
+				const { id } = jwt.verify(token, 'hello')
+				user = await prisma.user.findUnique({
+					where: { id },
+				})
+
+				if (!user) {
+					throw new Error('Not real user')
+				}
+			} catch (error) {
+				res.status(401)
+				res.json({ error: 'Now Authorized' })
+				return
+			}
+			return handler(req, res, user)
+		}
+
+		// If no token
+		res.status(401)
+		res.json({ error: 'Not Authorized' })
+	}
+}
