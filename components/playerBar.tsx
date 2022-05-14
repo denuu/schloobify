04b24b51f5fa236f0ece5033eba52b4981beb080
Denis Nossevitch <denis.nossevitch@gmail.com>
2022-05-13 22:33:46 -0700

    playerbar play states, but not quite working correctly

---
+++ components/playerBar.tsx
@@ -0,0 +1,28 @@
+import { Box, Flex, Text } from '@chakra-ui/layout'
+import Player from './player'
+import { useStoreState } from 'easy-peasy'
+
+const PlayerBar = () => {
+  const songs = useStoreState((state: any) => state.activeSongs)
+  const activeSong = useStoreState((state: any) => state.activeSongs)
+  return (
+    <Box height="100px" width="100vw" bg="grey.900" padding="10px">
+      <Flex align="center">
+        {activeSong ? (
+          <Box padding="20px" color="white" width="30%">
+            <Text fontSize="large">Song Name</Text>
+            <Text fontSize="sm">Artist Name</Text>
+          </Box>
+        ) : null}
+        <Box color="white" width="40%">
+          {activeSong ? <Player songs={songs} activeSong={activeSong} /> : null}
+        </Box>
+        <Box color="white" width="30%">
+          blah
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
+
+export default PlayerBar
--- components/songsTable.tsx
+++ components/songsTable.tsx
@@ -3,8 +3,17 @@ import { Table, Thead, Th, Tr, Td, Tbody, IconButton } from '@chakra-ui/react'
 import { BsFillPlayFill } from 'react-icons/bs'
 import { AiOutlineClockCircle } from 'react-icons/ai'
 import { formatDate, formatTime } from '../lib/formatters'
+import { useStoreActions } from 'easy-peasy'
 
 const SongTable = ({ songs }) => {
+  const playSongs = useStoreActions((store: any) => store.changeActiveSongs)
+  const setActiveSong = useStoreActions((store: any) => store.changeActiveSongs)
+
+  const handlePlay = (activeSong?) => {
+    setActiveSong(activeSong || songs[0])
+    playSongs(songs)
+  }
+
   return (
     <Box bg="transparent" color="white">
       <Box padding="10px" marginBottom="20px">
@@ -15,6 +24,7 @@ const SongTable = ({ songs }) => {
             colorScheme="green"
             size="lg"
             isRound
+            onClick={() => handlePlay()}
           />
           <Table variant="unstyled">
             <Thead
@@ -40,7 +50,8 @@ const SongTable = ({ songs }) => {
                     },
                   }}
                   key={song.id}
-                  cursor="cursor"
+                  cursor="pointer"
+                  onClick={() => handlePlay(song)}
                 >
                   <Td>{i + 1}</Td>
                   <Td>{song.name}</Td>
