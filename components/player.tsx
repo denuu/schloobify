eb557b3e508743941158e0fe93f594e8aeef03b3
Denis Nossevitch <denis.nossevitch@gmail.com>
2022-05-19 20:31:06 -0700

    player control hooks

--- components/player.tsx
+++ components/player.tsx
@@ -26,9 +26,11 @@ const Player = ({ songs, activeSong }) => {
   const [playing, setPlaying] = useState(true)
   const [index, setIndex] = useState(0) // active song index
   const [seek, setSeek] = useState(0.0) // seek position on progress bar
+  const [isSeeking, setIsSeeking] = useState(false)
   const [repeat, setRepeat] = useState(false)
   const [shuffle, setShuffle] = useState(false)
   const [duration, setDuration] = useState(0.0)
+  const soundRef = useRef(null) // Reference for react howler
 
   const setPlayState = (value) => {
     setPlaying(value)
@@ -42,9 +44,56 @@ const Player = ({ songs, activeSong }) => {
     setRepeat((state) => !state)
   }
 
+  const prevSong = () => {
+    setIndex((state) => {
+      return state ? state - 1 : songs.length - 1
+    })
+  }
+
+  const nextSong = () => {
+    setIndex((state) => {
+      // Account for shuffle, but not repeat.
+      if (shuffle) {
+        const next = Math.floor(Math.random() * songs.length)
+        if (next === state) {
+          return nextSong()
+        }
+        return next
+      }
+      return state === songs.length - 1 ? 0 : state + 1
+    })
+  }
+
+  const onEnd = () => {
+    if (repeat) {
+      soundRef.current.seek(0) // Set react howler to start of song.
+    } else {
+      nextSong()
+    }
+  }
+
+  const onLoad = () => {
+    const songDuration = soundRef.current.duration() // Howler function, instead of DB, ensure no mismatched times.
+    setDuration(songDuration)
+  }
+
+  const onSeek = (e) => {
+    setSeek(parseFloat(e[0])) // The chakra component spits out min and max values array.
+    soundRef.current.seek(e[0])
+  }
+
   return (
     <Box>
-      <Box>{/* <ReactHowler playing={playing} src={activeSong?.url} /> */}</Box>
+      <Box>
+        <ReactHowler
+          playing={playing}
+          src={activeSong?.url}
+          ref={soundRef}
+          onLoad={onLoad}
+          onEnd={onEnd}
+          // NOTE: Can accept volume prop for volume slider use!
+        />
+      </Box>
       <Center color="grey.600">
         <ButtonGroup>
           <IconButton
@@ -62,6 +111,7 @@ const Player = ({ songs, activeSong }) => {
             aria-label="skip previous"
             fontSize="24px"
             icon={<MdSkipPrevious />}
+            onClick={prevSong}
           />
           {playing ? (
             <IconButton
@@ -90,6 +140,7 @@ const Player = ({ songs, activeSong }) => {
             aria-label="ship next"
             fontSize="24px"
             icon={<MdSkipNext />}
+            onClick={nextSong}
           />
           <IconButton
             outline="none"
@@ -112,8 +163,12 @@ const Player = ({ songs, activeSong }) => {
               aria-label={['min', 'max']}
               step={0.1}
               min={0}
-              max={100}
               id="player-range"
+              max={duration ? duration.toFixed(2) : 0}
+              onChange={onSeek}
+              value={[seek]}
+              onChangeState={() => setIsSeeking(true)}
+              onChangeEnd={() => setIsSeeking(false)}
             >
               <RangeSliderTrack bg="grey.800">
                 <RangeSliderFilledTrack bg="grey.600" />
