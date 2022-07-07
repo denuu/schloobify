d0a8d024e9c2b5510f40c9dce1f14e3c87736c93
Denis Nossevitch <denis.nossevitch@gmail.com>
2022-07-06 21:11:32 -0700

    fix artists key issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ const Home = ({ artists }) => {
         </Box>
         <Flex>
           {artists.map((artist) => (
-            <Box paddingX="10px" width="20%">
+            <Box paddingX="10px" width="20%" key={artist.id}>
               <Box bg="grey.900" borderRadius="4px" padding="15px">
                 <Image
                   src="https://placekitten.com/300/300"
