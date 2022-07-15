0676bebc87d325d1ca86668383f1d07da68befeb
Denis Nossevitch <denis.nossevitch@gmail.com>
2022-07-14 20:53:46 -0700

    playlists added to homescreen for now

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import { Box, Text, Flex } from '@chakra-ui/layout'
 import { Image } from '@chakra-ui/react'
 import GradientLayout from '../components/gradientLayout'
-import { useMe } from '../lib/hooks'
+import { useMe, usePlaylist } from '../lib/hooks'
 import prisma from '../lib/prisma'
 
 const Home = ({ artists }) => {
   const { user } = useMe()
+  const { playlists } = usePlaylist()
 
   return (
     <GradientLayout
@@ -15,6 +16,7 @@ const Home = ({ artists }) => {
       title={`${user?.firstName} ${user?.lastName}`}
       description={`${user?.playlistsCount} public playlists`}
       image="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2"
+      icon={null}
     >
       <Box color="white" paddingY="40px">
         <Box marginBottom="40px">
@@ -34,6 +36,31 @@ const Home = ({ artists }) => {
                 <Box marginTop="20px">
                   <Text fontSize="large">{artist.name}</Text>
                   <Text fontSize="x-small">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
+
+      <Box color="white" paddingY="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Your Playlists
+          </Text>
+          <Text fontSize="md">Only visible to you</Text>
+        </Box>
+        <Flex>
+          {playlists.map((playlist) => (
+            <Box paddingX="10px" width="20%" key={playlist.id}>
+              <Box bg="grey.900" borderRadius="4px" padding="15px">
+                <Image
+                  src="https://placekitten.com/300/300"
+                  borderRadius="100%"
+                />
+                <Box marginTop="20px">
+                  <Text fontSize="large">{playlist.name}</Text>
+                  <Text fontSize="x-small">Playlist</Text>
                 </Box>
               </Box>
             </Box>
